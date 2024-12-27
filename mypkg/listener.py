48e35ccb16ccab2b4712da179fb97cf217b62d42
Satoru Homma <s23C1128DB@s.chibakoudai.jp>
2024-12-27 22:21:33 +0900

    remake

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,11 +5,12 @@ from std_msgs.msg import Int16
 rclpy.init()
 node = Node("listener")
 
+
 def cb(msg):
     global node
-    node.get_logger().info("listen: %d" % msg)
+    node.get_logger().info("Listen: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(Person, "countup", cb, 10)
+    sub = node.create_subscription(Int16, "countup", cb, 10)
     rclpy.spin(node)
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,19 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 rclpy.init()
-node = Node("talker")
+node = Node("takler")
 pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
+
 def cb():
     global n
-    msg = Person()
-    msg.name = "本間聖琉"
-    msg.age = n
+    msg = Int16()
+    msg.data = n
     pub.publish(msg)
     n += 1
+
 
 def main():
     node.create_timer(0.5, cb)
     rclpy.spin(node)
-
